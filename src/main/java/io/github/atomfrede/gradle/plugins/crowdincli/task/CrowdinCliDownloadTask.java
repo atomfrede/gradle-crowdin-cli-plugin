bb2fd88a95fa9cbd88411138849a7d5d784a7477
Frederik Hahne <atomfrede@gmail.com>
2017-10-23 21:19:25 +0200

    correct description

--- src/main/java/io/github/atomfrede/gradle/plugins/crowdincli/task/CrowdinCliDownloadTask.java
+++ src/main/java/io/github/atomfrede/gradle/plugins/crowdincli/task/CrowdinCliDownloadTask.java
@@ -7,7 +7,7 @@ import java.net.MalformedURLException;
 public class CrowdinCliDownloadTask extends Download {
 
     public static final String TASK_NAME = "downloadCrowdinCli";
-    public static final String DESCRIPTION = "Downloads the latest crowdin cli to into gradle/crowdin-cli.jar";
+    public static final String DESCRIPTION = "Downloads the latest crowdin cli to into gradle/X.X.X/crowdin-cli.jar";
 
     private static final String crowdinCliDownloadUrl = "https://downloads.crowdin.com/cli/v2/crowdin-cli.zip";
 

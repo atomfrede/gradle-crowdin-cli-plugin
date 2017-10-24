05014cbfb0d278ca51a07b9f33c4dee67815be79
Frederik Hahne <frederik.hahne@wallmedien.de>
2017-10-24 15:18:58 +0200

    prepare some stuff for configuration of crowdin cli interaction

--- src/main/java/io/github/atomfrede/gradle/plugins/crowdincli/task/crowdin/CrowdinDownloadSpec.java
+++ src/main/java/io/github/atomfrede/gradle/plugins/crowdincli/task/crowdin/CrowdinDownloadSpec.java
@@ -3,4 +3,6 @@ package io.github.atomfrede.gradle.plugins.crowdincli.task.crowdin;
 public interface CrowdinDownloadSpec extends CrowdinSpec {
 
     void dryRun(boolean dryRun);
+
+    boolean isDryRun();
 }
--- src/main/java/io/github/atomfrede/gradle/plugins/crowdincli/task/crowdin/CrowdinSpec.java
+++ src/main/java/io/github/atomfrede/gradle/plugins/crowdincli/task/crowdin/CrowdinSpec.java
@@ -1,12 +1,20 @@
 package io.github.atomfrede.gradle.plugins.crowdincli.task.crowdin;
 
 import java.io.File;
+import java.util.List;
 
+/**
+ * A simple config spec for tasks interacting with crowdin cli and a config file.
+ *
+ * If now config file is provided the default crowdin.yaml will be used.
+ */
 public interface CrowdinSpec {
 
     void configFile(File configFile);
 
     void identityFile(File identity);
+
+    void command(List<String> command);
 
 
 }
